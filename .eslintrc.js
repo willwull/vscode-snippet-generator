82e83ed0989d7e7b33bca4977fc0321c341662af
Willy Liu <arkazain@gmail.com>
2018-03-09 20:20:56 +0100

    🚧 Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    "extends": "airbnb", // for React
+    "env": {
+        "node": true,
+        "browser": true,
+    },
+    "rules": {
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "no-console": 0,
+        "no-underscore-dangle": 0,
+        "no-use-before-define": 0,
+        "no-plusplus": 0,
+        "quote-props": 0,
+        "react/jsx-filename-extension": 0,
+    },
+};
