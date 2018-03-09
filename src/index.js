aab843ddba08bbd7a4ce5cb437daec3d05e31314
Willy Liu <arkazain@gmail.com>
2018-03-09 21:24:51 +0100

    🚧 Move CSS

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { createStore } from "redux";
 import { Provider } from "react-redux";
 import reducer from "./reducers";
 import registerServiceWorker from "./registerServiceWorker";
-import "./index.css";
+import "./styles/index.css";
 
 const initialState = {
   name: "",
