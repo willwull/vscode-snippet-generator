19463d01debcb4b9cf5377f355fe4b015ef38111
Willy Liu <arkazain@gmail.com>
2018-03-10 15:56:53 +0100

    🌈 Add info text and update header

--- src/components/App.js
+++ src/components/App.js
@@ -8,6 +8,9 @@ function App() {
     <div>
       <header id="header">
         <h1>VS Code snippet generator</h1>
+        Fill out the form to generate a JSON snippet.
+        Double quotes are automatically escaped and the body is
+        automatically split into a string array.
       </header>
       <div id="main-container">
         <div id="input-container">
